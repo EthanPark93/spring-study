ae1696c5bf45fc5a211852966f19a9b51b9bf846
EthanPark93 <katsube01@gmail.com>
2023-07-21 11:02:26 +0900

    daily_commit

--- servlet/src/main/java/ethan/servlet/ServletApplication.java
+++ servlet/src/main/java/ethan/servlet/ServletApplication.java
@@ -2,6 +2,9 @@ package ethan.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
 
 @SpringBootApplication
 public class ServletApplication {
