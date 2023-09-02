c1e604f13f102fa3c553e4c4ebe4885e61c5723b
EthanPark93 <katsube01@gmail.com>
2023-09-02 19:08:10 +0900

    daily_commit

---
+++ servlet/src/main/java/ethan/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,27 @@
+package ethan.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Content-Type: text/html;charset=utf-8
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("  <div>이것은 단순 text가 아닌 html 코드.</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
