e49cbd5d994ea0e6461ff93e4d9e35a267f8ca0d
EthanPark93 <katsube01@gmail.com>
2023-09-17 19:26:00 +0900

    daily_commit

---
+++ servlet/src/main/java/ethan/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,30 @@
+package ethan.servlet.web.frontcontroller.v1.controller;
+
+import ethan.servlet.domain.member.Member;
+import ethan.servlet.domain.member.MemberRepository;
+import ethan.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관한다.
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
