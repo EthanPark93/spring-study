5dcd5a2681882ef8b8789f1fbc192175ada7d29d
EthanPark93 <katsube01@gmail.com>
2023-10-01 22:54:37 +0900

    daily_commit

---
+++ servlet/src/main/java/ethan/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -0,0 +1,9 @@
+package ethan.servlet.web.frontcontroller.v3;
+
+import ethan.servlet.web.frontcontroller.ModelView;
+
+import java.util.Map;
+
+public interface ControllerV3 {
+    ModelView process(Map<String, String> paramMap);
+}
---
+++ servlet/src/main/java/ethan/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -0,0 +1,13 @@
+package ethan.servlet.web.frontcontroller.v3.controller;
+
+import ethan.servlet.web.frontcontroller.ModelView;
+import ethan.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberFormControllerV3 implements ControllerV3 {
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return new ModelView("new-form");
+    }
+}
