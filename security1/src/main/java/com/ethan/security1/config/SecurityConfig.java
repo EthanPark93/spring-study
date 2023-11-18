c00df7a5cec5ce236df355ad37efd8ee630a0dd9
EthanPark93 <katsube01@gmail.com>
2023-11-18 21:32:35 +0900

    DELETE git cached

--- security1/src/main/java/com/ethan/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/ethan/security1/config/SecurityConfig.java
@@ -8,16 +8,18 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 //@EnableGlobalMethodSecurity // deprecated 됨. EnableMethodSecurity 를 대신 사용하라고 한다.
-@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true) // securedEnabled => Secured 애노테이션 사용 여부, prePostEnabled => PreAuthorize 어노테이션 사용 여부.
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+// securedEnabled => Secured 애노테이션 사용 여부, prePostEnabled => PreAuthorize 어노테이션 사용 여부.
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
         http.csrf(CsrfConfigurer::disable);
         http.authorizeHttpRequests(authorize ->
                 authorize
@@ -26,12 +28,16 @@ public class SecurityConfig {
                         .requestMatchers("/admin/**").hasAnyRole("ADMIN")
                         .anyRequest().permitAll()
         ).formLogin(formLogin ->
-                formLogin
-                        .loginPage("/loginForm")
+                        formLogin
+                                .loginPage("/loginForm")
 //                        .usernameParameter("파라미터 바꾸고싶으면 여기에 입력")
-                        .loginProcessingUrl("/login") // 시큐리티에서 낚아챔
-                        .defaultSuccessUrl("/")
+                                .loginProcessingUrl("/login") // 시큐리티에서 낚아챔
+                                .defaultSuccessUrl("/")
+        ).oauth2Login(oauth2Login ->
+                oauth2Login
+                        .loginPage("/loginForm")
         );
+//        .oauth2Login(Customizer.withDefaults());
 
         return http.build();
     }
