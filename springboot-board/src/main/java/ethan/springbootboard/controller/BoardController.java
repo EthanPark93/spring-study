586311ef1532eeedb5af4e7b6b1f962c30cf8c6f
EthanPark93 <katsube01@gmail.com>
2023-12-02 18:29:45 +0900

    boardPagingLogic

--- springboot-board/src/main/java/ethan/springbootboard/controller/BoardController.java
+++ springboot-board/src/main/java/ethan/springbootboard/controller/BoardController.java
@@ -4,6 +4,9 @@ import ethan.springbootboard.dto.BoardDTO;
 import ethan.springbootboard.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -70,5 +73,34 @@ public class BoardController {
 
         return "detail";
 //        return "redirect:/board/" + boardDTO.getId();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public void delete(@PathVariable Long id) {
+        boardService.delete(id);
+    }
+
+    // /board/paging?page=1
+    @GetMapping("/paging")
+    public String paging(@PageableDefault(page = 1) Pageable pageable, Model model) {
+//        pageable.getPageNumber();
+        Page<BoardDTO> boardList = boardService.paging(pageable);
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        // ex) page 갯수 20개
+        // 현재 사용자가 3페이지
+        // 1 2 '3'
+        // 현재 사용자가 7페이지
+        // '7' 8 9
+        // 보여지는 페이지 갯수 3개
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "paging";
     }
 }
--- springboot-board/src/main/java/ethan/springbootboard/service/BoardService.java
+++ springboot-board/src/main/java/ethan/springbootboard/service/BoardService.java
@@ -4,6 +4,11 @@ import ethan.springbootboard.dto.BoardDTO;
 import ethan.springbootboard.entity.BoardEntity;
 import ethan.springbootboard.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +19,7 @@ import java.util.Optional;
 // DTO -> Entity (Entiry Class)
 // Entity -> DTO (DTO Class)
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BoardService {
@@ -61,5 +67,34 @@ public class BoardService {
         boardRepository.save(boardEntity);
 
         return findById(boardDTO.getId());
+    }
+
+    public void delete(Long id) {
+        boardRepository.deleteById(id);
+    }
+
+    public Page<BoardDTO> paging(Pageable pageable) {
+
+        int page = pageable.getPageNumber() - 1;
+        int pageLimit = 3; // 한 페이지에 보여줄 글 갯수
+
+        // 한 페이지당 pageLimit 개씩 글을 보여주고 정렬 기준은 id 기준으로 내림차순 정렬
+        // page 위치의 매개변수는 0부터 시작 (인덱스 카운팅)
+        Page<BoardEntity> boardEntities =
+                boardRepository.findAll(PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
+
+        log.info("boardEntities.getContent() = {}", boardEntities.getContent()); // 요청 페이지에 해당하는 글
+        log.info("boardEntities.getTotalElements() = {}", boardEntities.getTotalElements()); // 전체 글갯수
+        log.info("boardEntities.getNumber() = {}", boardEntities.getNumber()); // DB로 요청한 페이지 번호
+        log.info("boardEntities.getTotalPages() = {}", boardEntities.getTotalPages()); // 전체 페이지 갯수
+        log.info("boardEntities.getSize() = {}", boardEntities.getSize()); // 한 페이지에 보여지는 글 갯수
+        log.info("boardEntities.hasPrevious() = {}", boardEntities.hasPrevious()); // 이전 페이지 존재 여부
+        log.info("boardEntities.isFirst() = {}", boardEntities.isFirst()); // 첫 페이지 여부
+        log.info("boardEntities.isLast() = {}", boardEntities.isLast()); // 마지막 페이지 여부
+
+        // 목록: id, writer, title, hits, createdTime
+        Page<BoardDTO> boardDTOS = boardEntities.map(board -> new BoardDTO(board.getId(), board.getBoardWriter(), board.getBoardTitle(), board.getBoardHits(), board.getCreatedTime()));
+
+        return boardDTOS;
     }
 }
